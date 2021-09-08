4610433472abf88be0bdcf7dc3bd1099882f8cc9
Mikhail Olusanya <mikhail.olusanya@gmail.com>
2021-09-08 08:47:24 +0100

    secretkey

--- polling/settings.py
+++ polling/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-#xt!r_w8%0ysqg5#xh$xgy0n0$=yt*9vs1!49no4(&%5vlstv0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
